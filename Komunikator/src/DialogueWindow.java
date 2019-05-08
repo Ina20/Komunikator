92ff9bf2266c2670c0b0b7229fd82433b8bb6483
panda020 <49525674+panda020@users.noreply.github.com>
2019-05-08 19:21:20 +0200

    Add files via upload

--- Komunikator/src/DialogueWindow.java
+++ Komunikator/src/DialogueWindow.java
@@ -1,16 +1,40 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class DialogueWindow {
 
     JFrame frame;
-    JTextArea textInput
+    JPanel panel2;
+    JTextArea textInput, area;
+    JButton sendButton;
 
     public void createWindow(){
 
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(600, 500));
+
+        area = new JTextArea();
+        area.setPreferredSize(new Dimension(600, 350));
+        area.setBackground(Color.PINK);
+        frame.add(area, BorderLayout.NORTH);
+
+        panel2 = new JPanel();
+        panel2.setPreferredSize(new Dimension(600, 100));
+        frame.add(panel2, BorderLayout.SOUTH);
+
+        textInput = new JTextArea();
+        textInput.setPreferredSize(new Dimension(400, 100));
+        textInput.setBackground(Color.CYAN);
+        panel2.add(textInput, new FlowLayout(FlowLayout.LEFT));
+
+        sendButton = new JButton("Wyślij");
+        sendButton.setPreferredSize(new Dimension(150, 80));
+        panel2.add(sendButton, new FlowLayout(FlowLayout.RIGHT));
+
+
+
         frame.pack();
         frame.setVisible(true);
     }
--- Komunikator/src/Main.java
+++ Komunikator/src/Main.java
@@ -1,9 +1,63 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
+    static String wiadomoscOdebrana = null, wiadomoscWysylana = null;
+    static PrintWriter pr;
+    static boolean connected = false;
+
 
     public static void main(String[] args) {
 
+        //tworzenie klienta
+        Socket s = null;
+        try {
+            s = new Socket("192.168.43.119", 4999); // change ip
+            //s = new Socket("localhost",4999);
+            pr = new PrintWriter( s.getOutputStream());
+            connected = true;
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
         DialogueWindow window = new DialogueWindow();
         window.createWindow();
 
+        InputStreamReader in = null;
+        BufferedReader bf = null;
+
+        //przycisk wyslij
+        window.sendButton.addActionListener( new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                wiadomoscWysylana = window.textInput.getText();
+                window.area.setText( "Client: " + wiadomoscWysylana );
+                System.out.println( wiadomoscWysylana );
+                pr.println( wiadomoscWysylana );
+                pr.flush();
+                window.textInput.setText( null );
+
+            }
+        } );
+//odbieranie wiadomosci
+        while (connected) {
+            try {
+                in = new InputStreamReader( s.getInputStream() );
+                bf = new BufferedReader( in );
+                wiadomoscOdebrana = bf.readLine();
+                window.area.setText( "Serwer: " + wiadomoscOdebrana );
+                System.out.println( "Serwer: " + wiadomoscOdebrana );
+                //wyswietlanie wiadomosci w oknie
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
     }
 }
