d07d6e587fc54d95bc38cfb241fcf057d37aec6d
Ina20 <49551246+Ina20@users.noreply.github.com>
2019-05-08 21:18:48 +0200

    Add files via upload

--- Komunikator/src/DialogueWindow.java
+++ Komunikator/src/DialogueWindow.java
@@ -5,10 +5,11 @@ import java.awt.*;
 public class DialogueWindow {
 
     JFrame frame;
-    JPanel panel2;
+    JPanel panel;
     JScrollPane inputScroll, displayScroll;
     JTextArea textInput, displayArea;
     JButton sendButton;
+    ImagePanel imgPanel;
 
     public void createWindow(){
 
@@ -16,34 +17,43 @@ public class DialogueWindow {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(600, 500));
 
+        imgPanel = new ImagePanel();
+        imgPanel.setPreferredSize(new Dimension(600, 350));
+
         displayArea = new JTextArea();
-        displayArea.setBackground(Color.PINK);
+        //displayArea.setBackground(Color.PINK);
         displayArea.setEditable(false);
         displayArea.setLineWrap(true);
         displayArea.setWrapStyleWord(true);
+        displayArea.setOpaque(false);
+        displayArea.setForeground(Color.WHITE);
 
         displayScroll = new JScrollPane(displayArea);
-        displayScroll.setPreferredSize(new Dimension(600, 350));
-        displayScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        frame.add(displayScroll, BorderLayout.NORTH);
+        displayScroll.setPreferredSize(new Dimension(590, 350));
+        displayScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        displayScroll.setOpaque(false);
+        displayScroll.getViewport().setOpaque(false);
+       // frame.add(displayScroll, BorderLayout.NORTH);
+        imgPanel.add(displayScroll);
+        frame.add(imgPanel, BorderLayout.NORTH);
 
-        panel2 = new JPanel();
-        panel2.setPreferredSize(new Dimension(600, 100));
-        frame.add(panel2, BorderLayout.SOUTH);
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(600, 110));
+        frame.add(panel, BorderLayout.SOUTH);
 
         textInput = new JTextArea();
-        textInput.setBackground(Color.CYAN);
+        textInput.setBackground(Color.LIGHT_GRAY);
         textInput.setLineWrap(true);
         textInput.setWrapStyleWord(true);
 
         inputScroll = new JScrollPane(textInput);
         inputScroll.setPreferredSize(new Dimension(400, 100));
-        inputScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        panel2.add(inputScroll, new FlowLayout(FlowLayout.LEFT));
+        inputScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        panel.add(inputScroll, new FlowLayout(FlowLayout.LEFT));
 
         sendButton = new JButton("Wyślij");
-        sendButton.setPreferredSize(new Dimension(150, 80));
-        panel2.add(sendButton, new FlowLayout(FlowLayout.RIGHT));
+        sendButton.setPreferredSize(new Dimension(130, 70));
+        panel.add(sendButton, new FlowLayout(FlowLayout.RIGHT));
 
 
 
